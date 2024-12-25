d8f803e050628519cb482acf0a5347163eced248
dklee <dklee7280@gmail.com>
2024-12-25 14:24:01 +0900

    새로운

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/joggi", // GitHub Pages 저장소 이름
+  assetPrefix: "/joggi", // GitHub Pages 저장소 이름
 };
 
 export default nextConfig;
+
+// 빈 export 구문 추가
+export {};
