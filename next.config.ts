ce90fac083d564785be856e2131be12a0a005184
dklee <dklee7280@gmail.com>
2024-12-25 14:46:07 +0900

    새로운

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "export",
-  basePath: "",
-  assetPrefix: "https://joggi.co.kr",
-  trailingSlash: true, // URL 끝에 슬래시 추가
 };
 
 export default nextConfig;
