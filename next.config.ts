9ef74f23834a324b567873e3dbdce0a3807b7ad6
dklee <dklee7280@gmail.com>
2024-12-27 08:07:39 +0900

    Add next.config.ts back to Git tracking

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+// const nextConfig = {
+//   output: "export",
+//   basePath: "",
+//   assetPrefix: "https://jogi.co.kr",
+//   trailingSlash: true,
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
+// export default nextConfig;
