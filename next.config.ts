c3d0f28e7d006612c398e57d145c50d475be8f56
dklee <dklee7280@gmail.com>
2024-12-26 22:12:18 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "",
-  assetPrefix: "https://jogi.co.kr",
-  trailingSlash: true,
-  reactStrictMode: true,
+  output: "server", // or "export" if you're using next export
+  basePath: "", // 서브 디렉토리 배포 시 추가
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://jogi.co.kr" : "", // CDN 설정
+  trailingSlash: true, // URL 끝에 슬래시 추가
+  reactStrictMode: true, // 개발 모드에서 React의 문제를 쉽게 찾을 수 있게 설정
   compiler: {
-    styledComponents: true,
+    styledComponents: true, // styled-components 사용 시
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // 이미지 최적화 하지 않음
   },
 };
 
