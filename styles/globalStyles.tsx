bfc33cd5b298f2c0b3f36e9e41f581f7054739da
dklee <dklee7280@gmail.com>
2024-12-28 14:30:26 +0900

    Update globalStyles.tsx

--- styles/globalStyles.tsx
+++ styles/globalStyles.tsx
@@ -55,15 +55,17 @@ const GlobalStyle = createGlobalStyle`
   }
 
   html, body {
-    width: 100vw;
-    height: 100vh;
-    font-family: "Paperlogy", "Pretendard", Arial, Helvetica, sans-serif;
-    overflow: hidden; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5f5f5; 
-  }
+    width: 100vw; /* 화면의 전체 너비를 차지 */
+    height: 100vh; /* 화면의 전체 높이를 차지 */
+    font-family: "Paperlogy", "Pretendard", Arial, Helvetica, sans-serif; /* 지정된 폰트 사용 */
+    overflow: hidden; /* 스크롤 제거 */
+    display: flex; /* 플렉스 컨테이너로 설정 */
+    align-items: center; /* 수직 방향으로 중앙 정렬 */
+    justify-content: center; /* 수평 방향으로 중앙 정렬 */
+    background-color: #f5f5f5; /* 배경색 설정 */
+    -webkit-overflow-scrolling: none; /* iOS에서 부드러운 스크롤 비활성화 */
+}
+
 
   body.fixed {
     position: fixed;
@@ -76,6 +78,7 @@ const GlobalStyle = createGlobalStyle`
     all: unset; 
     font-family: inherit; 
     font-size: inherit; 
+    -webkit-appearance: none; /* 기본 스타일 제거 */
   }
 `;
 
