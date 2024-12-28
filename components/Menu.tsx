ab64413d9c402010f32106bf7978d3fb263f8112
dklee <dklee7280@gmail.com>
2024-12-28 17:35:29 +0900

    [design] 메뉴 디자인 추가

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,10 +1,53 @@
 "use client";
+import { useState } from "react";
 import * as S from "../styles/menu";
+import Image from "next/image";
 export default function Menu() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <S.Menu>
-      <S.MenuContainer></S.MenuContainer>
-      <S.MenuOpen></S.MenuOpen>
+      {/* 메뉴 컨테이너 클릭 시 openMenu 상태 변경 */}
+      <S.MenuContainer onClick={() => setOpenMenu((prev) => !prev)}>
+        <S.MenuImageContainer>
+          <Image
+            src="/image/menu.png"
+            alt="로고 아이콘"
+            layout="fill"
+            objectFit="contain"
+          />
+        </S.MenuImageContainer>
+      </S.MenuContainer>
+
+      {/* openMenu가 true일 때만 MenuOpen 표시 */}
+      {openMenu && (
+        <S.MenuOpen>
+          <S.SubMenuImageContainer>
+            <Image
+              src="/image/vote.png"
+              alt="투표 메뉴"
+              layout="fill"
+              objectFit="contain"
+            />
+          </S.SubMenuImageContainer>
+          <S.SubMenuImageContainer>
+            <Image
+              src="/image/day.png"
+              alt="일 메뉴"
+              layout="fill"
+              objectFit="contain"
+            />
+          </S.SubMenuImageContainer>
+          <S.SubMenuImageContainer>
+            <Image
+              src="/image/month.png"
+              alt="월 메뉴"
+              layout="fill"
+              objectFit="contain"
+            />
+          </S.SubMenuImageContainer>
+        </S.MenuOpen>
+      )}
     </S.Menu>
   );
 }
--- styles/menu.tsx
+++ styles/menu.tsx
@@ -21,17 +21,38 @@ export const MenuContainer = styled.div`
   z-index: 20; /* MenuContainer가 위에 오도록 설정 */
 `;
 
+export const MenuImageContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative; /* Image에서 position 절대값 활용 */
+`;
+
+export const SubMenuImageContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative; /* Image에서 position 절대값 활용 */
+  margin-bottom: 10px;
+`;
+
 export const MenuOpen = styled.div`
   width: 50px; /* 네모박스의 너비 */
-  height: 100px; /* 네모박스의 높이 */
+  height: 170px; /* 네모박스의 높이 */
   background-color: #ff9a2f; /* 네모박스의 배경색 */
   position: relative;
-  top: calc(90% - 125px);
+  top: calc(90% - 190px);
   left: 80%;
   border-radius: 8px; /* 모서리 둥글게 */
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
   z-index: 10; /* MenuContainer보다 뒤에 배치 */
+  padding-bottom: 30px;
+  padding-top: 10px;
 `;
